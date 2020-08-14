d9bbc719ed5487c11e11a52bf6868fe5d90775e8
albertHuang <yuda.h.ncku@gmail.com>
2020-08-14 17:57:10 +0800

    import spring boot as a spring boot application

---
+++ management-system/src/main/java/com/albert/management/ManagementApplication.java
@@ -0,0 +1,12 @@
+package com.albert.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        SpringApplication.run(ManagementApplication.class, args);
+    }
+}
