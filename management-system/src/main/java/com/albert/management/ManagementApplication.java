4de91e0dc396e1a810a905ea65a59d24a0071f5d
alberthyd <47311038+alberthyd@users.noreply.github.com>
2020-08-14 17:22:32 +0800

    Revert "Spring 4 using web starter"

--- management-system/src/main/java/com/albert/management/ManagementApplication.java
+++ management-system/src/main/java/com/albert/management/ManagementApplication.java
@@ -4,8 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ManagementApplication {
-    public static void main(String[] args) {
+public class ManagementApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        // 執行 Spring Boot
         SpringApplication.run(ManagementApplication.class, args);
     }
 }
