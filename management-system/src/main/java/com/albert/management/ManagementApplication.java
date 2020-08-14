985c56c98b9182e29dee3f5caae78cba13c3fa57
albertHuang <yuda.h.ncku@gmail.com>
2020-08-14 18:01:42 +0800

    using spring-boot-starter-web to develop api in controller

--- management-system/src/main/java/com/albert/management/ManagementApplication.java
+++ management-system/src/main/java/com/albert/management/ManagementApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ManagementApplication {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         SpringApplication.run(ManagementApplication.class, args);
     }
 }
