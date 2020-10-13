a8d7360cb9ae5ff6ea98b4bd97ef2ebc1bbd550b
albertHuang <yuda.h.ncku@gmail.com>
2020-10-13 12:25:08 +0800

    spring boot default logging dependencies introducing (slf4j-api & log4j-api).

--- management-system/src/main/java/com/albert/management/ManagementApplication.java
+++ management-system/src/main/java/com/albert/management/ManagementApplication.java
@@ -5,7 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ManagementApplication {
+
+    private static final org.slf4j.Logger logger_slf4j = org.slf4j.LoggerFactory.getLogger(ManagementApplication.class);
+    private static final org.apache.logging.log4j.Logger logger_log4j = org.apache.logging.log4j.LogManager.getLogger(ManagementApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(ManagementApplication.class, args);
+        logger_slf4j.info("Hi, {}", "SLF4J");
+        logger_log4j.info("Hi, {}", "LOG4J");
     }
 }
