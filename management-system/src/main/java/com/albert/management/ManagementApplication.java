acb9449986e2527c59edd68d779cf7a1b436c31a
albertHuang <yuda.h.ncku@gmail.com>
2020-11-04 19:02:33 +0800

    - import spring-boot-devtools dependency on dev environment in pom.xml - turn on the properties of devtools.restart.enabled - turn off the properties of devtools.livereload.enabled - turn off the properties of devtools.remote.enabled - remove log4j2 logging dependencies and related setting

--- management-system/src/main/java/com/albert/management/ManagementApplication.java
+++ management-system/src/main/java/com/albert/management/ManagementApplication.java
@@ -1,16 +1,13 @@
 package com.albert.management;
 
-// import org.slf4j.Logger;
-// import org.slf4j.LoggerFactory;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ManagementApplication {
-  // private static final Logger log = LoggerFactory.getLogger(ManagementApplication.class);
-  private static final Logger log = LogManager.getLogger(ManagementApplication.class);
+  private static final Logger log = LoggerFactory.getLogger(ManagementApplication.class);
 
   public static void main(String[] args) {
     SpringApplication.run(ManagementApplication.class, args);
