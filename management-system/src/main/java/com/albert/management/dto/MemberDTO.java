f5079865f282f317abbb15f4f800acb4e2fc21c4
albertHuang <yuda.h.ncku@gmail.com>
2021-07-01 17:00:46 +0800

    - import spring-data-jpa - add jpa's properties - add MemberDTO and mapping to the table 'member' - modify createSequence's startValue and incrementBy value

---
+++ management-system/src/main/java/com/albert/management/dto/MemberDTO.java
@@ -0,0 +1,48 @@
+package com.albert.management.dto;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "member")
+public class MemberDTO {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequence_generator")
+    @SequenceGenerator(name = "sequence_generator")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", name='" + getName() + "'" +
+            "}";
+    }
+
+}
